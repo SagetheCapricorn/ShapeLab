d3e15fa662881691ba6f54e511c67490a6d33c81
BT_1N3_12 <jtan5662@bths.edu>
2018-10-16 14:48:55 -0400

    pewpew

---
+++ src/ETriangle.java
@@ -0,0 +1,10 @@
+public class ETriangle {
+    private int side;
+
+    public ETriangle (int side) {
+        this.side = side
+    }
+    public double getArea() {
+        return ((Math.sqrt(3)/4)*
+    }
+}
