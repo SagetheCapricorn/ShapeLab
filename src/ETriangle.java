2148f4ee61828c031ea99408aad8e48e126896dc
jason tan <jtan5662@bths.edu>
2018-10-17 14:42:38 -0400

    uptown girl

--- src/ETriangle.java
+++ src/ETriangle.java
@@ -2,9 +2,12 @@ public class ETriangle {
     private int side;
 
     public ETriangle (int side) {
-        this.side = side
+        this.side = side;
     }
     public double getArea() {
-        return ((Math.sqrt(3)/4)*
+        return ((Math.sqrt(3)/4)*Math.pow(side,2));
+    }
+    public double getPerimeter() {
+        return side*3;
     }
 }
---
+++ src/Runner.java
@@ -0,0 +1,5 @@
+public class Runner {
+    public static void main(String[] args) {
+        Shape shape1 = new Rectangle
+    }
+}
